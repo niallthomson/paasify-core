5d0bac74e56e0671dcdfcd9b65d97f25f64c59a6
Niall Thomson <nthomson@pivotal.io>
2019-09-28 10:32:54 -0700

    Adding module to support building network configuration

---
+++ build-network-config/aws/main.tf
@@ -0,0 +1,18 @@
+data "template_file" "subnets" {
+  template = "${chomp(file("${path.module}/templates/subnet-config.yml"))}"
+
+  count = length(var.subnet_azs)
+
+  vars = {
+    id       = "${var.subnet_ids[count.index]}"
+    dns      = "${cidrhost(var.vpc_cidr, 2)}"
+    cidr     = "${var.subnet_cidrs[count.index]}"
+    gateway  = "${cidrhost(var.subnet_cidrs[count.index], 1)}"
+    reserved = "${cidrhost(var.subnet_cidrs[count.index], 0) }-${cidrhost(var.subnet_cidrs[count.index], 9) }"
+    az       = "${var.subnet_azs[count.index]}"
+  }
+}
+
+locals {
+  subnet_config = "[${join("," data.template_file.subnets.*.rendered)}]"
+}
---
+++ build-network-config/aws/vars.tf
@@ -0,0 +1,13 @@
+variable "vpc_cidr" {} 
+
+variable "subnet_ids" {
+  type = list(string)
+} 
+
+variable "subnet_cidrs" {
+  type = list(string)
+} 
+
+variable "subnet_azs" {
+  type = list(string)
+}
