5e4afef3a6005253b4a7db8b5eedb6d701b38843
Niall Thomson <nthomson@pivotal.io>
2020-02-22 17:03:24 -0800

    force delete bucket

--- infra/aws/buckets.tf
+++ infra/aws/buckets.tf
@@ -4,7 +4,8 @@ resource "random_integer" "bucket_suffix" {
 }
 
 resource "aws_s3_bucket" "ops_manager_bucket" {
-  bucket = "${var.environment_name}-ops-manager-bucket-${random_integer.bucket_suffix.result}"
+  bucket        = "${var.environment_name}-ops-manager-bucket-${random_integer.bucket_suffix.result}"
+  force_destroy = true
 
   tags = merge(
     var.tags,
