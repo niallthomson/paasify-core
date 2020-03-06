df21669ae038d0118d576b7355f9aed92b04ed0c
Niall Thomson <nthomson@pivotal.io>
2020-03-06 10:55:52 -0800

    Fix apply changes deleting resources at the wrong time

--- apply-changes/main.tf
+++ apply-changes/main.tf
@@ -1,3 +1,23 @@
+resource "null_resource" "destroy" {
+  count = var.auto_apply ? 1 : 0
+
+  provisioner "remote-exec" {
+    inline = ["echo ${var.blocker}"]
+  }
+
+  provisioner "remote-exec" {
+    when = destroy
+
+    inline = ["wrap destroy_opsman"]
+  }
+
+  connection {
+    host        = var.provisioner_host
+    user        = var.provisioner_ssh_username
+    private_key = var.provisioner_ssh_private_key
+  }
+}
+
 resource "null_resource" "apply_changes" {
   count = var.auto_apply ? 1 : 0
 
@@ -6,13 +26,7 @@ resource "null_resource" "apply_changes" {
   }
 
   provisioner "remote-exec" {
-    inline = ["wrap apply_changes ${var.blocker}"]
-  }
-
-  provisioner "remote-exec" {
-    when = destroy
-
-    inline = ["wrap destroy_opsman"]
+    inline = ["wrap apply_changes ${null_resource.destroy.id}"]
   }
 
   connection {
