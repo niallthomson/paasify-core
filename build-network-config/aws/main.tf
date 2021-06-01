1f444702617296d55a14397fd3087bd678b528b5
Niall Thomson <thomson.niall@gmail.com>
2021-06-01 08:51:42 -0600

    Update TF syntax

--- build-network-config/aws/main.tf
+++ build-network-config/aws/main.tf
@@ -4,15 +4,15 @@ data "template_file" "subnets" {
   count = length(var.subnet_azs)
 
   vars = {
-    id       = "${var.subnet_ids[count.index]}"
-    dns      = "${cidrhost(var.vpc_cidr, 2)}"
-    cidr     = "${var.subnet_cidrs[count.index]}"
-    gateway  = "${cidrhost(var.subnet_cidrs[count.index], 1)}"
+    id       = var.subnet_ids[count.index]
+    dns      = cidrhost(var.vpc_cidr, 2)
+    cidr     = var.subnet_cidrs[count.index]
+    gateway  = cidrhost(var.subnet_cidrs[count.index], 1)
     reserved = "${cidrhost(var.subnet_cidrs[count.index], 0)}-${cidrhost(var.subnet_cidrs[count.index], 9)}"
-    az       = "${var.subnet_azs[count.index]}"
+    az       = var.subnet_azs[count.index]
   }
 }
 
 locals {
-  subnet_config = "[${join(",", data.template_file.subnets.*.rendered)}]"
+  subnet_config = [join(",", data.template_file.subnets.*.rendered)]
 }
