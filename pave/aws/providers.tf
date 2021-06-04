73a1cfba0591f8d0ace06497c7b22e9b0d8609df
Niall Thomson <thomson.niall@gmail.com>
2021-06-04 15:20:11 -0600

    Updated provider syntax

--- pave/aws/providers.tf
+++ pave/aws/providers.tf
@@ -1,19 +1,41 @@
 terraform {
   required_version = ">= 0.14"
+
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 2.1.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 2.2.0"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 2.1.0"
+    }
+
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.1.0"
+    }
+  }
 }
 
 provider "null" {
-  version = "~> 2.1.0"
+
 }
 
 provider "random" {
-  version = "~> 2.2.0"
+
 }
 
 provider "tls" {
-  version = "~> 2.1.0"
+
 }
 
 provider "template" {
-  version = "~> 2.1.0"
+
 }
